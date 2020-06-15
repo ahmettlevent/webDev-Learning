aac01b552dbaac94ef356a0e417fa3ec1f0cf432
Ahmet Utku Levent <ahmettlevent@gmail.com>
2020-06-15 13:04:04 +0300

    re-edit-files

--- Async/github api/js/app.js
+++ Async/github api/js/app.js
@@ -1,11 +1,3 @@
-// SELECTORS
-const countryImg = document.querySelector("#country-img")
-const selectCountry = document.querySelector("#selectCountry")
-const textArea = document.querySelector("#textarea")
-const resultArea = document.querySelector("#result-p")
-
-const ui = new UI()
-const requests = new TranstlateRequest()
 
 // Call From Start Funcs
 changeCountry()
@@ -19,18 +11,3 @@ function eventListeners() {
 }
 
 // Func's
-function changeCountry(e) {
-  ui.changeCountry(selectCountry, countryImg)
-  search()
-}
-
-function search(e) {
-  let language = ui.getCountry(selectCountry)
-  let text = ui.getText(textArea)
-  
-  if (text.length > 0) {
-    requests.get(language, text)
-      .then(data => ui.changeResult(resultArea, data))
-      .catch(err => console.log("hata"))
-  }
-}
--- Async/github api/js/requests.js
+++ Async/github api/js/requests.js
@@ -1,9 +1,3 @@
-class TranstlateRequest {
-  async get(lang, text) {
-    const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180930T080756Z.753c49142579b043.b2798189b8760e7b357c9d23a8736ef0a54be481&text=${text}&lang=${lang}`
-
-    const myresponse = await fetch(url)
-    return myresponse.text();
-  }
+class GithubRequest {
 
 }
