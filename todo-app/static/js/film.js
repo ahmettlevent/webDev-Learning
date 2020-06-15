830bcf8147cce39f5042d32a7286d8378602044a
Ahmet Utku Levent <ahmettlevent@gmail.com>
2020-06-16 02:41:41 +0300

    todo app removed

--- todo-app/static/js/film.js
+++
@@ -1,7 +0,0 @@
-class Movie {
-    constructor(MovieName, MovieDirector, MovieLink) {
-        this.MovieName = MovieName
-        this.MovieDirector = MovieDirector
-        this.MovieLink = MovieLink
-    }
-}
