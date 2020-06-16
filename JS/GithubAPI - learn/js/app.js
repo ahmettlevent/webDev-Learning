6f8a368fe8a86276b288f238eba4921027ae8a3a
Ahmet Utku Levent <ahmettlevent@gmail.com>
2020-06-16 13:17:05 +0300

    interface fix

--- JS/GithubAPI - learn/js/app.js
+++ JS/GithubAPI - learn/js/app.js
@@ -1,6 +1,8 @@
 // Elements
+let form_username = document.getElementById("form-username")
 
 let input_username = document.getElementById("input-username")
+let search_username = document.getElementById("search-username")
 
 let user_name = document.getElementById("user-name")
 let user_img = document.getElementById("user-img")
@@ -15,6 +17,7 @@ let user_id = document.getElementById("user-id")
 
 // non-useable words
 const unused_words = ["ş", "ğ", "ü", "ç", "İ", "Ç", "Ü", "Ğ"]
+
 // Object's
 const request = new GithubRequest()
 const ui = new UI()
@@ -23,11 +26,10 @@ const ui = new UI()
 eventListeners()
 
 function eventListeners() {
-	input_username.addEventListener("input", newUserName)
+	form_username.addEventListener("submit", newUserName)
 }
 
 // Func's
-
 function newUserName(e) {
 	let username = input_username.value
 	if ((username).length > 0 && !(unused_words.indexOf(username) >= 0)) {
@@ -35,6 +37,8 @@ function newUserName(e) {
 			.then(data => UserToUI(JSON.parse(data)))
 			.catch(err => console.log("yarra yedig"))
 	}
+
+	e.preventDefault()
 }
 
 function UserToUI(newUser) {
