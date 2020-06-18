8966d3ac0dba658c9ffc839bc684193491626402
Ahmet Utku Levent <ahmettlevent@gmail.com>
2020-06-19 00:14:03 +0300

    MASTER

--- Nodejs/question-answerAPI/routers/question.js
+++ Nodejs/question-answerAPI/routers/question.js
@@ -1,11 +1,10 @@
 const express = require("express")
-
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Questions homepage")
-})
 
+})
 
 router.get("/delete", (req, res) => {
   res.send("Questions homepage")
